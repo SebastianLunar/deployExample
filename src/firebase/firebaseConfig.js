4162e09512668fe3cc9eba1cfec1b193c2b04dc7
Sebastian Luna <selunar@outlook.com>
2024-11-06 21:59:14 -0500

    update firebaseconfig

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBicCRow16KH8BHDI2hPU56xGj7TMUsPpM",
   authDomain: "workoutapp-a582d.firebaseapp.com",
   projectId: "workoutapp-a582d",
-  storageBucket: "workoutapp-a582d.appspot.com",
+  storageBucket: "workoutapp-a582d.firebasestorage.app",
   messagingSenderId: "854224472820",
   appId: "1:854224472820:web:6cf738ea868d751e1eaca1"
 };
